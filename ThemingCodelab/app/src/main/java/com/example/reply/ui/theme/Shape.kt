346beb10279f2ea3f8a14f9784e1fcbd15de3ebf
Seops34 <tjtjdgur34@gmail.com>
2024-05-01 18:10:13 +0900

    [CodeLab] Themeing in Compose with Material3

---
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.example.reply.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shape = Shapes(
+    extraSmall = RoundedCornerShape(size = 4.dp),
+    small = RoundedCornerShape(size = 8.dp),
+    medium = RoundedCornerShape(size = 16.dp),
+    large = RoundedCornerShape(size = 25.dp),
+    extraLarge = RoundedCornerShape(size = 32.dp)
+)
